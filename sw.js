9186c85cc5255b0f5d6f1c9cbbfb03ab55db20f8
Edward Leung <edward_leung_12@hotmail.com>
2020-09-01 14:21:48 +0100

    pre-caching assets

--- sw.js
+++ sw.js
@@ -1,4 +1,40 @@
-// Install service worker
+// cache name
+const staticCacheName = "site-static";
+// array of asset to cache
+const assets = [
+  // store request url
+  ["/"],
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://use.fontawesome.com/releases/v5.14.0/css/all.css",
+];
+
+// Install service worker event (when file changes)
 self.addEventListener("install", (evt) => {
-  console.log("service worker has been installed");
+  //console.log("service worker has been installed");
+  // it doesn't finish the install event until this promise is resolved
+  evt.waitUntil(
+    // access caches api, it's asycn task
+    caches.open("staticCacheName").then((cache) => {
+      // add item to the cache using assets array
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate service worker event
+self.addEventListener("activate", (evt) => {
+  //console.log("service worker has been activated");
+});
+
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  //console.log("fetch event", evt);
 });
