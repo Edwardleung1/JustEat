5a94a72b9b8cc2fd20a732998aca3c8e100690b0
Edward Leung <edward_leung_12@hotmail.com>
2020-09-01 23:27:55 +0100

    added dynamic caching in offline mode

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 // cache name
-const staticCacheName = "site-static";
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v1";
+
 // array of asset to cache
 const assets = [
   // store request url
@@ -23,7 +25,7 @@ self.addEventListener("install", (evt) => {
   // it doesn't finish the install event until this promise is resolved
   evt.waitUntil(
     // access caches api, it's asycn task
-    caches.open("staticCacheName").then((cache) => {
+    caches.open(staticCacheName).then((cache) => {
       // add item to the cache using assets array
       console.log("caching shell assets");
       cache.addAll(assets);
@@ -34,6 +36,21 @@ self.addEventListener("install", (evt) => {
 // activate service worker event
 self.addEventListener("activate", (evt) => {
   //console.log("service worker has been activated");
+  // it doesn't finish the activate event until this promise is resolved
+  evt.watchUntil(
+    //asyn tasks that returns a promise
+    caches.keys().then((keys) => {
+      // it will get the keys and take it in the callback function
+      // console.log(keys);
+      return Promise.all(
+        keys
+          // take it an array of promises
+          .filter((key) => key !== staticCacheName) // if the key is not equal then goes to filter arr
+          // delete the cache that is passed (old cache)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 // fetch event
@@ -44,7 +61,17 @@ self.addEventListener("fetch", (evt) => {
     // see if a match with request
     caches.match(evt.request).then((cacheRes) => {
       // return cache if we have it or the initial fetch request
-      return cacheRes || fetch(evt.request);
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          // store dynamic cache
+          return caches.open(dynamicCacheName).then((cache) => {
+            // store the new response in the cache
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
