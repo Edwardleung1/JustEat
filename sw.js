5615b3da69694e798af38897dae3bbc354cc96c1
Edward Leung <edward_leung_12@hotmail.com>
2020-09-02 15:00:45 +0100

    limited the cache size file.

--- sw.js
+++ sw.js
@@ -20,6 +20,21 @@ const assets = [
   "/pages/fallback.html",
 ];
 
+// limit cache size function
+const limitCacheSize = (name, size) => {
+  // open whatever cache we put in
+  caches.open(name).then((cache) => {
+    // get keys of the cache
+    cache.keys().then((keys) => {
+      // we have an array of keys
+      if (keys.length > size) {
+        // delete first item in the cache array, and call function again.
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 // Install service worker event (when file changes)
 self.addEventListener("install", (evt) => {
   //console.log("service worker has been installed");
@@ -69,14 +84,22 @@ self.addEventListener("fetch", (evt) => {
           fetch(evt.request).then((fetchRes) => {
             // store dynamic cache
             return caches.open(dynamicCacheName).then((cache) => {
-              // store the new response in the cache
+              // store the new response in the dynamic cache
               cache.put(evt.request.url, fetchRes.clone());
+              // check if cache is over limit size
+              limitCacheSize(dynamicCacheName, 15);
               return fetchRes;
             });
           })
         );
         // serve up the fallback page if the fetch fails
       })
-      .catch(() => caches.match("/pages/fallback.html"))
+      .catch(() => {
+        // searches the string .html and returns an integer position
+        if (evt.request.url.indexOf(".html") > -1) {
+          // indeed a html page we are requesting
+          return caches.match("/pages/fallback.html");
+        }
+      })
   );
 });
